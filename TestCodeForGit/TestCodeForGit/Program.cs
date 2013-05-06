ebe27a9f3aef0e460243b78165a385164526967e
Yaron Ben Shalom <yaron.b@tm-it.co.il>
2013-05-06 14:31:08 +0300

    This is a test commit form VS2012

---
+++ TestCodeForGit/TestCodeForGit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCodeForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a test for GIT");
+        }
+    }
+}
